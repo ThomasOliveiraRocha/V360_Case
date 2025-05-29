27b7f1a0eac0843a41824dc4467b8e0c0c10efc1
ThomasOliveiraRocha <57138761+ThomasOliveiraRocha@users.noreply.github.com>
2025-05-28 23:23:05 -0300

    gambiarra para poder compilar o seed e popular o banco, pois so usuarios pagantes pode usar o terminal no render

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from models import db
 from routes import api_bp
 from dotenv import load_dotenv
+from seed import seed
 import os
 
 load_dotenv()
@@ -32,6 +33,14 @@ def not_found(e):
 @app.errorhandler(500)
 def internal_error(e):
     return jsonify({'status': 'error', 'message': 'Internal server error'}), 500
+
+@app.route('/run-seed')
+def run_seed():
+    try:
+        seed()
+        return jsonify({'message': '🌱 Banco populado com sucesso!'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=False)
